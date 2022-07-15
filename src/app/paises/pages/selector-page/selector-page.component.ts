83a25f3eaa0b7af4ee1e8f7298388fafecfd30e7
Alexander <alexander.martinez@lapatota.cl>
2022-07-14 23:15:36 -0400

    Selector de regiones

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
   selector: 'app-selector-page',
@@ -11,9 +12,17 @@ export class SelectorPageComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required]
   })
-  constructor(private fb: FormBuilder) { }
+
+  //lenar selectores
+  regiones: string[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private ps: PaisesService
+  ) { }
 
   ngOnInit(): void {
+    this.regiones = this.ps.regiones;
   }
 
   guardar() {
